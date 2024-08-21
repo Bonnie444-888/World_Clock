fd212e120bb62ee8b5535d52693968d65431f18f
Bonnie444-888 <isabel.kedibone@gmail.com>
2024-08-21 12:41:26 +0200

    Added interaction in JS using moment JS and updated HTML file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+
+  let windhoekElement = document.querySelector("#windhoek");
+  let windhoekDateElement = windhoekElement.querySelector(".date");
+  let winhoekTimeElement = windhoekElement.querySelector(".time");
+  let windhoekTime = moment().tz("Africa/Windhoek");
+
+  windhoekDateElement.innerHTML = windhoekTime.format("MMMM Do YYYY");
+  winhoekTimeElement.innerHTML = `${windhoekTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+}
+updateTime();
+setInterval(updateTime, 1000);
